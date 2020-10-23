1fb81b4f7011882791a45171770a14e94de8580e
Tulika28 <trudragupta@iitkgp.ac.in>
2020-10-23 12:01:48 +0530

    this is the assignment of caching the inverse of a matrix

---
+++ assign_week3.R
@@ -0,0 +1,29 @@
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
+cacheSolve <- function(x, ...) {
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
+}
+
+matx <- matrix(c(1,3,9,7),2,2)
+
+matx1 <- makeCacheMatrix(matx)
+cacheSolve(matx1) 
